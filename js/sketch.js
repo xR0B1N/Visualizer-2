83d8165e7d8167f704ce82ac0c2e19054fefd98c
DarkPhotonBeam <DanPhoton@yahoo.com>
2020-02-18 01:56:59 +0100

    Comments + Updates

--- js/sketch.js
+++ js/sketch.js
@@ -1,16 +1,12 @@
 //import * as THREE from "./lib/threejs/three";
 
-// "Shortcuts"
-// let sin = Math.sin;
-// let cos = Math.cos;
-// let tan = Math.tan;
 const pi = Math.PI;
 
-let sound, amplitude;
+let sound, amplitude, fft, spectrum;
 
 // p5 sound preload
 function preload() {
-    sound = loadSound('https://dev.darkphotonbeam.com/audio/test/rain.mp3');
+    sound = loadSound('https://dev.darkphotonbeam.com/audio/bruhEP/resurrected.mp3');
 }
 
 // p5 setup
@@ -19,6 +15,7 @@ function setup() {
     masterVolume(1);
     sound.play();
     amplitude = new p5.Amplitude();
+    fft = new p5.FFT();
     console.log("p5 SETUP");
 
 
@@ -43,21 +40,26 @@ function asEnvelopeFS(signal, attack, sustain, steep) {
     }
 }
 
+/// THREE.JS SCENE SETUP
+
+const FOV = 50; // DEF: 75
+
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+let camera = new THREE.PerspectiveCamera( FOV, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 let renderer = new THREE.WebGLRenderer({
     antialias: true
 });
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-// $( window ).resize(  );
+// Resize canvas on window resize
 window.addEventListener("resize", function() {
     camera.aspect = window.innerWidth/window.innerHeight;
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 });
+
 
 document.body.appendChild( renderer.domElement );
 
@@ -73,14 +75,13 @@ let material = new THREE.MeshBasicMaterial( {
 } );
 
 
-let objArr = [];
-let origin = {
+let objArr = []; // All cubes/meshes are stored in here for later processing
+let origin = { // Center of the circle
     x: 0,
     y: 0,
     z: -30
 };
 
-let ttt = 0;
 
 function addObjects(geo, mat, num, amp, offAng) {
 
@@ -108,21 +109,21 @@ function addObjects(geo, mat, num, amp, offAng) {
 
 }
 
-
+// Ring settings
 const OBJ_NUM = 16;
-const OBJ_AMP = 1.5;
+const OBJ_AMP = 2;
 const OBJ_OFF = 0;
 
-
-addObjects(geometry, material, OBJ_NUM, OBJ_AMP, OBJ_OFF);
-// addObjects(geometry, material, 12, 2.5, pi / 12);
+addObjects(geometry, material, OBJ_NUM, OBJ_AMP, OBJ_OFF); // First summon
 
 
+// Camera position init
 camera.position.z = 5;
 
 let angleAdd = 0.0;
 ampAdd = 0;
 
+/// COUNTERS FOR TIME RELATED ACTIONS
 let tcount01 = 0;
 const TCOUNT01 = 1;
 
@@ -135,57 +136,76 @@ let then = 0.0;
 var clock = new THREE.Clock();
 
 let levelSamples = [];
-//let sampleCounter = 0;
-const SAMPLE_BUFFER = 1000;
+const SAMPLE_BUFFER = 1000; // Buffer size, the bigger the more accurate the normalisation is
 
+let level = 0;
 let loudest = 0;
 let quietest = 1;
-let normalizationBoost = 0;
+let normalisationBoost = 0;
+
+// Normalisation controls
 const NORM_BIAS = 0.1;
 const NORM_LEVEL = 0.3;
 const MIN_SAMPLES = 100;
 
+// How much the signal should be compressed if over the threshold
 const COMP_RATIO = 0.8;
 
+// Controls how much the "zoom" is influenced by the level
 const SPEED_RAMP_EXP = 1.4;
 const SPEED_RAMP_OFF = 0.1;
 const SPEED_RAMP_BIAS = 90;
 const SPEED_RAMP_BIAS2 = 0.05;
 let speedRampBias = 1;
 
+// How fast the cubes rotate around the center
 const ANGLE_ROTATION = 0.3;
 
+// Controls how much the level influences the cube size
 const SIDE_AMP_EXP = 1.5;
 const SIDE_AMP_OFF = 0.1;
 const SIDE_AMP_BIAS = 20;
 
+// Controls how much the level controls the circle radius, usually creates a rumble effect
 const AMP_EXP = 1.5;
 const AMP_OFF = 0.1;
 const AMP_BIAS = 5;
 
+// Controls the smoothness of level influence
+const FADE_IN = 0.8;
+const FADE_OUT = 0.2;
+
+// Controls spectrum range
+const SPEC_MIN = 120;
+const SPEC_MAX_FACT = 0.2;
+
+// Controls the spectrum amp
+const SPEC_SIDE_FACT = 10;
 
 let animate = function (now) {
-    //console.clear();
     // PRE
-    let width = window.innerWidth;
-    let height = window.innerHeight;
     const delta = clock.getDelta();
-    //then = now;
-    //ttt = now;
 
-    let level = amplitude.getLevel();
+    spectrum = fft.analyze();
+
+    // Keep buffer from overflowing
+    let newLevel = amplitude.getLevel();
+
+    let levelDif = newLevel - level;
+    if (levelDif >= 0) {
+        level += levelDif * FADE_IN;
+    } else {
+        level += levelDif * FADE_OUT;
+    }
+
     if (levelSamples.length === SAMPLE_BUFFER) {
         levelSamples.splice(0, 1);
-        //console.log("BUFFER FILLED");
     }
     levelSamples.push(level);
-    //sampleCounter++;
-    //console.log(level);
 
-    //ampAdd = 1*sin(now * 0.001) + 1;
-    angleAdd += ANGLE_ROTATION * delta;
-    //console.log(angleAdd);
+    angleAdd += ANGLE_ROTATION * delta; // For rotation-speed around centre
 
+    // Summon mesh ring all TCOUNT01 seconds*level
     if (tcount01 > TCOUNT01) {
         addObjects(geometry, material, OBJ_NUM, OBJ_AMP, OBJ_OFF);
         tcount01 = 0;
@@ -193,11 +213,10 @@ let animate = function (now) {
 
     function normalize() {
         let ratio = NORM_LEVEL / loudest;
-        //console.log(loudest);
         if (ratio < 1) {
-            normalizationBoost = ratio * COMP_RATIO;
+            normalisationBoost = ratio * COMP_RATIO;
         } else {
-            normalizationBoost = ratio;
+            normalisationBoost = ratio;
         }
 
         tcount02 = 0;
@@ -212,6 +231,7 @@ let animate = function (now) {
         quietest = level;
     }
 
+    // Check dynamics and normalize
     if (tcount02 > TCOUNT02) {
         // GET LOUDES VOLUME AND SET NORMALIZATION
         for (let s = 0; s < levelSamples.length; s++) {
@@ -221,32 +241,24 @@ let animate = function (now) {
         normalize();
     }
 
-
-
-    //console.log(normalizationBoost);
     if (levelSamples.length > MIN_SAMPLES) {
-        level *= normalizationBoost;
+        level *= normalisationBoost;
         let dynamicRange = loudest - quietest;
         let dynamicRatio = 1 / dynamicRange;
         speedRampBias = dynamicRatio * SPEED_RAMP_BIAS2;
         speedRampBias = 1;
     }
 
-
-
-    //console.log("NORMALIZED LEVEL: " + level);
-
     requestAnimationFrame( animate );
 
     const rotAdd = 0.01;
 
-
-
-    // MAIN
+    // Actions for each mesh
     for (let i = 0; i < objArr.length; i++) {
         let c = objArr[i];
         let phi = c.angle + angleAdd;
         let a = c.amp + ampAdd + AMP_BIAS * Math.pow(level + AMP_OFF, AMP_EXP);
+
         c.mesh.rotation.z = -phi;
         c.mesh.rotation.x += rotAdd;
         c.mesh.rotation.y += rotAdd;
@@ -254,14 +266,28 @@ let animate = function (now) {
         c.mesh.position.y = a * Math.cos(phi);
         c.mesh.position.z += delta * SPEED_RAMP_BIAS * Math.pow(level * speedRampBias+SPEED_RAMP_OFF, SPEED_RAMP_EXP);
 
-        //c.material.color = new THREE.Color(i / objArr.length, 0.1, 0); // 0.1, sweep(now * 0.00072) * 0.6
         let distRat = 1-((camera.position.z - c.mesh.position.z) / (camera.position.z - origin.z));
         c.opacity = asEnvelopeFS(distRat, 0.7, 1, 2);
 
         c.color[2] = 1-level;
         c.color[0] = level;
 
-        let sideAmp = Math.pow(level + SIDE_AMP_OFF, SIDE_AMP_EXP) * SIDE_AMP_BIAS + 1;
+        let phiMod = phi % (2 * pi);
+        let freq = 0;
+        let specMax = spectrum.length * SPEC_MAX_FACT;
+
+        if (phiMod <= pi) {
+            freq = map(phiMod, 0, pi, SPEC_MIN, specMax);
+        } else {
+            freq = map(phiMod, pi, 2*pi, specMax, SPEC_MIN);
+        }
+
+        freq = Math.floor(freq);
+
+        c.color[1] = spectrum[freq] / 255;
+
+
+        let sideAmp = 1 + Math.pow(level + SIDE_AMP_OFF, SIDE_AMP_EXP) * SIDE_AMP_BIAS + (spectrum[freq] / 255) * SPEC_SIDE_FACT;
 
         c.mesh.scale.x = sideAmp;
         c.mesh.scale.y = sideAmp;
@@ -282,9 +308,6 @@ let animate = function (now) {
             objArr.splice(i, 1);
         }
     }
-
-    //console.log(asEnvelopeFS(objArr[0].mesh.position.z / camera.z, 0.2, 1, 2));
-    //console.log(objArr[0].mesh.position.z / camera.position.z);
 
     renderer.render( scene, camera );
 
